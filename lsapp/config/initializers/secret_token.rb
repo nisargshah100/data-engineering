f167f5ceb0717f5bbf5f08293307d5208fecbdca
Nisarg Shah <nisargshah100@gmail.com>
2011-11-23 01:13:14 -0500

    initial project

---
+++ lsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsapp::Application.config.secret_token = 'a5796ca3e4c3f574261148ff88ca1c2d91290a8caa7ba90406c23cdccb63e4b236bad2956faa99700efe6190385719bd76c65cdf22eb4d3225a0b3fb4663e1eb'
